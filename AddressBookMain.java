fa1c5e38ce97858dae5c7b6795177293f8ea5515
akshat1032 <akshat.30oct@gmail.com>
2020-09-27 18:50:20 +0530

    Added : Add a new contact to address book added

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,11 +1,71 @@
 package com.capgemini.addressbooksystem;
 
+import java.util.Scanner;
+
+
 public class AddressBookMain {
 	
 	public static void main(String[] args) {
 		
-		//Creating a contact in Address Book
-		ContactPerson personObject = new ContactPerson("Tom", "Sawyer", "Los Angeles, California", "Beverly Hills", 90035L, 629483567L, "tom.sawyer@gmail.com");
+		//Local variables
+		String firstName;
+		String lastName;
+		String address;
+		String cityName;
+		long zipCode;
+		long phoneNumber;
+		String email;
+		
+		Scanner sc = new Scanner(System.in);
+		boolean defaultOption = true;
+		int personObjectCounter = 0;
+		
+		ContactPerson personObject[] = new ContactPerson[1000];//Assuming maximum no. of records to be stored is 1000
+		
+		//Loop to perform multiple operations based on user choice
+		while (defaultOption) {
+			
+			System.out.println("Enter the details of the person to be added");
+			sc.nextLine();
+			System.out.println("First name : ");
+			firstName = sc.nextLine();
+			System.out.println("Last name : ");
+			lastName = sc.nextLine();
+			System.out.println("Address : ");
+			address = sc.nextLine();
+			System.out.println("City Name : ");
+			cityName = sc.nextLine();
+			System.out.println("Zip Code : ");
+			zipCode = sc.nextLong();
+			System.out.println("Phone number : ");
+			phoneNumber = sc.nextLong();
+			sc.nextLine();
+			System.out.println("Email : ");
+			email = sc.nextLine();
+			
+			//Storing the contact details in Address Book
+			personObject[personObjectCounter] = new ContactPerson();
+			personObject[personObjectCounter].setFirstName(firstName);
+			personObject[personObjectCounter].setLastName(lastName);
+			personObject[personObjectCounter].setAddress(address);
+			personObject[personObjectCounter].setCityName(cityName);
+			personObject[personObjectCounter].setZipCode(zipCode);
+			personObject[personObjectCounter].setPhoneNumber(phoneNumber);
+			personObject[personObjectCounter].setEmail(email);
+			
+			//Asking for user's choice
+			System.out.println("Do you wish to add another record?(Y/N)");
+			char userChoice = sc.next().charAt(0);
+			if (userChoice == 'Y' || userChoice == 'y') {
+				personObjectCounter ++;
+				defaultOption = true;
+				continue;
+			}
+			else if (userChoice == 'N' || userChoice == 'n') {
+				defaultOption = false;
+			}
+			else
+				System.exit(0);
+		}
 	}
-
 }
--- ContactPerson.java
+++ ContactPerson.java
@@ -11,16 +11,46 @@ public class ContactPerson {
 	private long phoneNumber;
 	private String email;
 	
-	//Constructor to initialize the Address Book
-	public ContactPerson(String firstName,String lastName, String address, String cityName, long zipCode, long phoneNumber, String email) {
-		
-		// TODO Auto-generated constructor stub
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
 		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
 		this.address = address;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
 		this.cityName = cityName;
+	}
+	public long getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(long zipCode) {
 		this.zipCode = zipCode;
+	}
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
 		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
 		this.email = email;
 	}
 }
