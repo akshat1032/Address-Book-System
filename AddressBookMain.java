917a9f665a8661fd820d6017ad74869a4ec0af18
akshat1032 <akshat.30oct@gmail.com>
2020-09-27 17:31:41 +0530

    Added : Welcome message added

---
+++ AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.capgemini.addressbooksystem;
+
+public class AddressBookMain {
+	
+	public static void main(String[] args) {
+		
+		//Welcome message
+		System.out.println("Welcome to Address Book Program");
+	}
+
+}
